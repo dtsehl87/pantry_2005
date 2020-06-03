f46582046ce1e7f5216e62aa26a4fd706c97e7f7
Dan Sehl <dan.sehl@gmail.com>
2020-06-03 10:48:21 -0600

    Write recipe ingredients test and method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -7,4 +7,16 @@ class Recipe
     @ingredients_required = {}
   end
 
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required[ingredient].nil?
+      @ingredients_required[ingredient] = quantity
+    else
+      @ingredients_required[ingredient] += quantity
+    end
+  end
+
+  def ingredients
+    @ingredients_required.keys
+  end
+
 end
