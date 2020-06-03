326c06fa463bc1b7ce3bd5941f9fed62ff9c5762
Dan Sehl <dan.sehl@gmail.com>
2020-06-03 10:33:48 -0600

    Write restock test and passing methods for pantry

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -6,4 +6,17 @@ class Pantry
     @stock = {}
   end
 
+  def stock_check(ingredient)
+    return 0 if @stock.empty?
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, quantity)
+    if @stock.empty?
+      @stock[ingredient] = quantity
+    else
+      @stock[ingredient] += quantity
+    end
+  end
+
 end
